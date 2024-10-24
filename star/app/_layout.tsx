9411993d26f31d5a1611b8d914927de8d16d1d97
dylyou <dyluri007@gmail.com>
2024-10-24 10:52:07 -0700

    changed up the spacing

--- star/app/_layout.tsx
+++ star/app/_layout.tsx
@@ -16,76 +16,72 @@ const Tab = createBottomTabNavigator();
 export default function Layout() {
   return (
     <CurrentUser>
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: true,
-      }}
-    >
-      <Tab.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Icon name="home-outline" color={color} size={size} />
-                ), 
-              }}
-        
-
-
-      />
-
-            <Tab.Screen
-              name="login"
-              component={LoginScreen}
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Icon name="home-outline" color={color} size={size} />
-                ), 
-              }}
-        
-
-
-      />
-      <Tab.Screen
-        name="list"
-        component={ListScreen} 
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="person-outline" color={color} size={size} />
-          ),
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: true,
         }}
-      />
+      >
+        <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Icon name="home-outline" color={color} size={size} />
+                  ), 
+                }}
+        />
+
+        <Tab.Screen
+          name="login"
+          component={LoginScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home-outline" color={color} size={size} />
+            ), 
+          }}
+        />
+
+        <Tab.Screen
+          name="list"
+          component={ListScreen} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="person-outline" color={color} size={size} />
+            ),
+          }}
+        />
 
 
-{/* <Tab.Screen
-        name="user"
-        component={UserScreen} 
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="person-outline" color={color} size={size} />
-          ),
-        }}
-      /> */}
-      <Tab.Screen
-        name="WishLists"
-        component={RootLayout}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="search" color={color} size={size} />
-          ),
-        }}
-      /> 
+  {/* <Tab.Screen
+          name="user"
+          component={UserScreen} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="person-outline" color={color} size={size} />
+            ),
+          }}
+        /> */}
 
-      <Tab.Screen
-        name="settings"
-        component={SettingsScreen}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="settings-outline" color={color} size={size} />
-          ),
-        }}
-      />
+        <Tab.Screen
+          name="WishLists"
+          component={RootLayout}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="search" color={color} size={size} />
+            ),
+          }}
+        /> 
+
+        <Tab.Screen
+          name="settings"
+          component={SettingsScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="settings-outline" color={color} size={size} />
+            ),
+          }}
+        />
     </Tab.Navigator>
     </CurrentUser>
   );
