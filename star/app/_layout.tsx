7d5a45154135edcbc3aa031c4c12ab2ff6328a3e
nickGar123 <131065825+nickGar123@users.noreply.github.com>
2024-10-24 10:31:37 -0700

    Loginbcrypt (#24)
    * testing functionality
    
    * Updated to login
    
    * Login works. Attempting to get user_id
    
    * Login
    
    * Final login now works

--- star/app/_layout.tsx
+++ star/app/_layout.tsx
@@ -2,14 +2,17 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './Home';
 import SettingsScreen from './settings';
 import UserScreen from './user';
+import LoginScreen from './login';
 import ItemSearch  from './ItemSearch';
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { CurrentUser } from '@/components/Currentuser';
 
 const Tab = createBottomTabNavigator();
 
 export default function Layout() {
   return (
+    <CurrentUser>
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
@@ -19,6 +22,18 @@ export default function Layout() {
       <Tab.Screen
               name="Home"
               component={HomeScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Icon name="home-outline" color={color} size={size} />
+                ), 
+              }}
+        
+
+
+      />
+            <Tab.Screen
+              name="login"
+              component={LoginScreen}
               options={{
                 tabBarIcon: ({ color, size }) => (
                   <Icon name="home-outline" color={color} size={size} />
@@ -59,5 +74,6 @@ export default function Layout() {
         }}
       />
     </Tab.Navigator>
+    </CurrentUser>
   );
 }
